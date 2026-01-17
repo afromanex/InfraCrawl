bbbbfdf79844f313385f24348dfef4570b182a39
Afromanex <jensenj08@gmail.com>
2026-01-16 23:47:51 -0500

    chore: remove crawl_registry shim file

--- infracrawl/services/crawl_registry.py
+++
@@ -1,6 +0,0 @@
-"""Backward-compatible shim: re-export new crawl_registry package objects."""
-
-from infracrawl.services.crawl_registry.models import CrawlHandle, CrawlRecord  # noqa: F401
-from infracrawl.services.crawl_registry.registry import InMemoryCrawlRegistry  # noqa: F401
-
-__all__ = ["CrawlRecord", "CrawlHandle", "InMemoryCrawlRegistry"]
